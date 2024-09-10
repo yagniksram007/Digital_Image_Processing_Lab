97b16a310b61f07cc8cb9ea1e149435ccd4bfb90
Yagnik <yagniksram@gmail.com>
2024-09-10 15:31:15 +0530

    new commit

---
+++ Split_image.py
@@ -0,0 +1,24 @@
+import cv2
+
+image_path = 'Miguel.jpeg'
+image = cv2.imread(image_path)
+if image is None:
+    print("Error: Unable to load image.")
+    exit()
+
+print(image.shape)
+height, width, _ = image.shape
+
+center_y, center_x = height // 2, width // 2
+
+top_left = image[0:center_y, 0:center_x]
+top_right = image[0:center_y, center_x:]
+bottom_left = image[center_y:, 0:center_x]
+bottom_right = image[center_y:, center_x:]
+
+cv2.imshow('Top Left', top_left)
+cv2.imshow('Top Right', top_right)
+cv2.imshow('Bottom Left', bottom_left)
+cv2.imshow('Bottom Right', bottom_right)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
